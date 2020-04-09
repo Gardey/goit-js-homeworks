6891813ed8c3199785d5a405f5d23315965d5e32
  Max Gordiychuk <maxgordiychuk@MacBook-Pro-Max.local>
2020-04-09 20:08:22 +0300

    hw5

---
+++ hw5/js/hw5-task3.js
@@ -0,0 +1,39 @@
+
+class Storage {
+	constructor([ ...args ]) {
+		this.items = args;
+	}
+
+	getItems() {
+		console.log(this.items);
+	}
+
+	addItem(string) {
+		this.items.push(string);
+	}
+
+	removeItem(string) {
+		for (let i = 0; i < this.items.length; i += 1) {
+			if (this.items[i] === string) {
+				this.items.splice(i, 1);
+			}
+		}
+	}
+}
+
+
+const storage = new Storage([
+    'Нанитоиды',
+    'Пролонгер',
+    'Железные жупи',
+    'Антигравитатор',
+  ]);
+  
+  const items = storage.getItems();
+  console.table(items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор" ]
+  
+  storage.addItem('Дроид');
+  console.table(storage.items); // [ "Нанитоиды", "Пролонгер", "Железные жупи", "Антигравитатор", "Дроид" ]
+  
+  storage.removeItem('Пролонгер');
+  console.table(storage.items); // [ "Нанитоиды", "Железные жупи", "Антигравитатор", "Дроид" ]
